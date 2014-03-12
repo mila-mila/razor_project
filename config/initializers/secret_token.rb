c63827821cfadf3c369c52227badcc80ea372ed4
mila-mila <popovic.mila.mila@gmail.com>
2014-03-12 13:51:26 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RazorProject::Application.config.secret_token = '8c9e588a625a190cb0d9a86ebd41efeccf92b372da97a57cf452cd771a074f2c0bd9b544b8a17a4c94a919152e47f0ca0656b6148b0902d65c39304805f0db6e'
